3fb93af9cb9914ccb43219e941332c7faa46d3dd
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-24 17:36:26 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -14,7 +14,7 @@ class ArtistsController < ApplicationController
 	def create
 	  @artist = Artist.new(post_params(:name, :bio))
 	  @artist.save
-	  redirect_to post_path(@artist)
+	  redirect_to artist_path(@artist)
 	end
 
 	def edit
