75cda8fa9ea1bd895f0fb3e8027f100cc08a71f6
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-24 17:45:28 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
 	def update
 	  @song = Song.find(params[:id])
 	  @song.update(post_params(:name, :artist_id, :genre_id))
-	  redirect_to genre_path(@song)
+	  redirect_to song_path(@song)
 	end
 
 	private
