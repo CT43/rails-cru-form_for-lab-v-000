7994e445c528a29695f259b515195e61d44a0317
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-24 16:53:30 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -23,7 +23,7 @@ class ArtistsController < ApplicationController
 
 	def update
 	  @artist = Artist.find(params[:id])
-	  @artist.update(post_params(:name))
+	  @artist.update(post_params(:name, :bio))
 	  redirect_to post_path(@artist)
 	end
 
